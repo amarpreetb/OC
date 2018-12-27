80813146618686bdd0b9c1bf4f2c44f9464ae4d5
amarpreet bhachu <amarpreet.bhachu@gmail.com>
2018-12-27 09:46:20 -0500

    2018-12-27_09-45-54

--- js/main.js
+++ js/main.js
@@ -1,22 +1,68 @@
 var rows = document.querySelector('table').querySelectorAll("[class^='chainrow aright']");
 
 
-console.log(rows);
+// console.log(rows);
+var callssAbove1K = 0;
+var putssAbove1K = 0;
+var callsOpenintsAbove1K = 0;
+var putsOpenintsAbove1K = 0;
+
+var totalCallsAbove1KVolume = 0;
+var totalPutsAbove1KVolume = 0;
+
+//totals
+var totalCallsVolume = 0;
+var totalPutsVolume = 0;
+var totalCallsOpenInt = 0;
+var totalPutsOpenInt = 0;
+
 for(var i = 0; i < rows.length; i++){
-    console.log(parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')));
+    // console.log(parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')));
+
+    totalCallsVolume += parseInt(rows[i].cells[3].innerHTML.replace(/,/g, ''));
+    totalPutsVolume += parseInt(rows[i].cells[11].innerHTML.replace(/,/g, ''));
+    
+    // totalCallsOpenInt += rows[i].cells[6].innerHTML;
+    totalCallsOpenInt += parseInt(rows[i].cells[6].innerHTML.replace(/,/g, ''));
+    totalPutsOpenInt += parseInt(rows[i].cells[14].innerHTML.replace(/,/g, ''));
+
+    //calls
     if(parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')) > 1000){
         rows[i].cells[3].style.backgroundColor = "green"
+        totalCallsAbove1KVolume += parseInt(rows[i].cells[3].innerHTML.replace(/,/g, ''));
+        callssAbove1K++;
     }
+
+    //puts
     if(parseInt(rows[i].cells[11].innerHTML.replace(/,/g, '')) > 1000){
         rows[i].cells[11].style.backgroundColor = "green"
+        totalPutsAbove1KVolume += parseInt(rows[i].cells[11].innerHTML.replace(/,/g, ''));
+        putssAbove1K++
     }
 
-    //open int
-    if(parseInt(rows[i].cells[4].innerHTML.replace(/,/g, '')) > 1000){
-        rows[i].cells[4].style.backgroundColor = "green"
+    //calls OI
+    if(parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) > 1000){
+        rows[i].cells[6].style.backgroundColor = "green"
+        callsOpenintsAbove1K++;
     }
 
+    //puts OI
     if(parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 1000){
         rows[i].cells[14].style.backgroundColor = "green"
+        putsOpenintsAbove1K++;
     }
 }
+
+console.log("Calls above 1K:" + callssAbove1K);
+console.log("Puts above 1K:" + putssAbove1K);
+console.log("Calls above 1K OI:" + callsOpenintsAbove1K);
+console.log("Puts above 1K OI:" + putsOpenintsAbove1K);
+
+console.log("Calls above 1K Total:" + totalCallsAbove1KVolume);
+console.log("Puts above 1K Total:" + totalPutsAbove1KVolume);
+
+console.log("TOTAL");
+console.log("Total Calls Volume:" + totalCallsVolume);
+console.log("Total Puts Volume:" + totalPutsVolume);
+console.log("Total Calls Open Interest:" + totalCallsOpenInt);
+console.log("Total Puts Open Interest:" + totalPutsOpenInt);
