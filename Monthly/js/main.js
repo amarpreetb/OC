0aa168f18cfc730af28d4f770532a043e3b6ac54
amarpreet bhachu <amarpreet.bhachu@gmail.com>
2019-03-30 12:42:50 -0400

    2019-03-30_12-41-59

--- Monthly/js/main.js
+++ Monthly/js/main.js
@@ -14,6 +14,8 @@ var totalPutsVolume = 0;
 var totalCallsOpenInt = 0;
 var totalPutsOpenInt = 0;
 
+var tempmax = 0
+
 for(var i = 0; i < rows.length; i++){
     // console.log(parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')));
 
@@ -23,130 +25,6 @@ for(var i = 0; i < rows.length; i++){
     // totalCallsOpenInt += rows[i].cells[6].innerHTML;
     totalCallsOpenInt += parseInt(rows[i].cells[6].innerHTML.replace(/,/g, ''));
     totalPutsOpenInt += parseInt(rows[i].cells[14].innerHTML.replace(/,/g, ''));
-
-    //calls > 1000 ------------------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')) > 1000){
-        rows[i].cells[3].style.backgroundColor = "#80ff80"
-        totalCallsAbove1KVolume += parseInt(rows[i].cells[3].innerHTML.replace(/,/g, ''));
-        callssAbove1K++;
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[11].innerHTML.replace(/,/g, '')) > 1000){
-        rows[i].cells[11].style.backgroundColor = "#80ff80"
-        totalPutsAbove1KVolume += parseInt(rows[i].cells[11].innerHTML.replace(/,/g, ''));
-        putssAbove1K++
-    }
-
-    //calls > 1000 ... < 5000 ---------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')) > 5000 && parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')) < 10000){
-        rows[i].cells[3].style.backgroundColor = "#008000"
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[11].innerHTML.replace(/,/g, '')) > 5000 && parseInt(rows[i].cells[11].innerHTML.replace(/,/g, '')) > 10000){
-        rows[i].cells[11].style.backgroundColor = "#008000"
-    }
-
-    //calls > 10000 ... < 20000 ---------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')) > 10000 && parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')) < 20000){
-        rows[i].cells[3].style.backgroundColor = " #b38600"
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[11].innerHTML.replace(/,/g, '')) > 10000 && parseInt(rows[i].cells[11].innerHTML.replace(/,/g, '')) > 20000){
-        rows[i].cells[11].style.backgroundColor = " #b38600"
-    }
-
-    //calls > 20000 ... < 30000 ---------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')) > 20000 && parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')) < 30000){
-        rows[i].cells[3].style.backgroundColor = "#e65c00"
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[11].innerHTML.replace(/,/g, '')) > 20000 && parseInt(rows[i].cells[11].innerHTML.replace(/,/g, '')) > 30000){
-        rows[i].cells[11].style.backgroundColor = "#e65c00"
-    }
-
-    //calls > > 3000 ---------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[3].innerHTML.replace(/,/g, '')) > 30000){
-        rows[i].cells[3].style.backgroundColor = "#b30000"
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[11].innerHTML.replace(/,/g, '')) > 30000){
-        rows[i].cells[11].style.backgroundColor = "#b30000"
-    }
-
-    //========================================================================================
-
-    // //calls OI
-    // if(parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) > 1000){
-    //     // rows[i].cells[6].style.backgroundColor = "#768976"
-    //     callsOpenintsAbove1K++;
-    // }
-
-    // //puts OI
-    // if(parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 1000){
-    //     // rows[i].cells[14].style.backgroundColor = "#768976"
-    //     putsOpenintsAbove1K++;
-    // }
-
-    //=====
-
-    //calls > 1000 ------------------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) > 1000){
-        rows[i].cells[6].style.backgroundColor = "#80ff80"
-        totalCallsAbove1KVolume += parseInt(rows[i].cells[3].innerHTML.replace(/,/g, ''));
-        callsOpenintsAbove1K++;
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 1000){
-        rows[i].cells[14].style.backgroundColor = "#80ff80"
-        totalPutsAbove1KVolume += parseInt(rows[i].cells[11].innerHTML.replace(/,/g, ''));
-        putsOpenintsAbove1K++;
-    }
-
-    //calls > 1000 ... < 5000 ---------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) > 5000 && parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) < 10000){
-        rows[i].cells[6].style.backgroundColor = "#008000"
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 5000 && parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 10000){
-        rows[i].cells[14].style.backgroundColor = "#008000"
-    }
-
-    //calls > 10000 ... < 20000 ---------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) > 10000 && parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) < 20000){
-        rows[i].cells[6].style.backgroundColor = " #b38600"
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 10000 && parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 20000){
-        rows[i].cells[14].style.backgroundColor = " #b38600"
-    }
-
-    //calls > 20000 ... < 30000 ---------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) > 20000 && parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) < 30000){
-        rows[i].cells[6].style.backgroundColor = "#e65c00"
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 20000 && parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 30000){
-        rows[i].cells[14].style.backgroundColor = "#e65c00"
-    }
-
-    //calls > > 3000 ---------------------------------------------------------------------------------
-    if(parseInt(rows[i].cells[6].innerHTML.replace(/,/g, '')) > 30000){
-        rows[i].cells[6].style.backgroundColor = "#b30000"
-    }
-
-    //puts
-    if(parseInt(rows[i].cells[14].innerHTML.replace(/,/g, '')) > 30000){
-        rows[i].cells[14].style.backgroundColor = "#b30000"
-    }
 }
 
 document.getElementById("calls1k").innerHTML = "Calls above 1K: " + callssAbove1K;
